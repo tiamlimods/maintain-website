5f7fedb91f3d034afbefedd14e7e2dd2ea1456d1
xinvxueyuan <xinvxueyuan@yeah.net>
2025-05-05 13:16:21 +0800

    QX

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,4 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-module.exports = {
-  output: 'export',
-  basePath: '/maintain-site', // 替换为你的仓库名称
-  assetPrefix: '/maintain-site/', // 替换为你的仓库名称
-}
 export default nextConfig;
